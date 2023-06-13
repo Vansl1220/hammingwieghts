a675ac7336c6d3bf954366122a60f6de4e78a43c
Vansl1220 <wi22b110@technikum-wien.at>
2023-06-13 11:34:30 +0200

    Initial commit

---
+++ src/main/java/com/example/hammingweights1/Hammingweights1Application.java
@@ -0,0 +1,13 @@
+package com.example.hammingweights1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hammingweights1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hammingweights1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hammingweights1/Hammingweights1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hammingweights1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hammingweights1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
